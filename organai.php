73a77935521bb7f86f65266e5d5c501631c61a47
rfl84 <95877776+rfl84@users.noreply.github.com>
2022-09-17 18:48:34 +0300

    Add files via upload

---
+++ organai.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="refresh" content="1; url=http://autoorganizerrain.xfastcart.com/r1/?lnk=2751&s=Wxlc62p2?sub1=<?=$_GET[‘utm_source’]?>&sub2=<?=$_GET[‘utm_medium’]?>&sub3=<?=$_GET[‘utm_campaign’]?>&sub4=<?=$_GET[‘utm_content’]?>&sub5=<?=$_GET[‘utm_term’]?>">
+    </head>
+    <body>
+        
+    </body>
+</html>
